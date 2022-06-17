acd84e2dd5603ad3378806c7a4adeb371f354d3b
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-06-17 18:36:22 -0300

    1 file modified

--- src/app/thaylla-tasks.service.ts
+++ src/app/thaylla-tasks.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ThayllaTasksService {
+  listaTasks = [];
 
-  constructor() { }
+  constructor() {}
 
+  adicionar(task: string) {
+    this.listaTasks.push(task);
+  }
+
+  remover(index: number) {
+    this.listaTasks.splice(index, 1);
+  }
+
+  mostrarLista() {
+    return this.listaTasks;
+  }
+
+  contarTarefas() {
+    return this.listaTasks.length;
+  }
 }
