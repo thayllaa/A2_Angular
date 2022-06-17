c90689a70a3aed4417721ea10e6e721bf45fc954
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-06-17 18:21:54 -0300

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ThayllaHomeComponent } from './thaylla-home/thaylla-home.component';
 import { ThayllaTasksComponent } from './thaylla-tasks/thaylla-tasks.component';
+import { ThayllaTasksService } from './thaylla-tasks.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
-    {path: '', component: ThayllaHomeComponent},
-    {path: 'tasks', component: ThayllaTasksComponent}
-  ]) ],
-  declarations: [ AppComponent, HelloComponent, ThayllaHomeComponent, ThayllaTasksComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot([
+      {path: '', component: ThayllaHomeComponent},
+      {path: 'tasks', component: ThayllaTasksComponent}
+  ]) 
+],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    ThayllaHomeComponent, 
+    ThayllaTasksComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [ ThayllaTasksService ]
 })
 export class AppModule { }
